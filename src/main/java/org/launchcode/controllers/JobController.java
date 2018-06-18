8d57cebd9e82464a0616e3dac576b6c237d08188
carlgillis <carldgillis@gmail.com>
2018-06-18 14:32:10 -0500

    completed TODO #6 // All features work.  Mission accomplished!!!

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -9,8 +9,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-
 import javax.validation.Valid;
 
 /**
